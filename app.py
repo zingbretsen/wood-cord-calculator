d92178c927d4da02b61b75bbb2cbfc69d4de5c2e
Zach Ingbretsen <zach@zingbretsen.com>
2022-03-05 17:13:28 -0500

    Adjusts values and rounding

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 st.title('Wood Size Calculator')
 
 cords = st.slider('cords', min_value=1, max_value=5, value=4)
-rows = st.slider('rows', min_value=1, max_value=5, value=4)
+rows = st.slider('rows', min_value=1, max_value=12, value=4)
 height = st.slider('height', min_value=4.0, max_value=6.0, step=0.5, value=4.0)
 
 depth = rows * 16
@@ -13,6 +13,6 @@ depth_w_gaps = rows * 16 + (rows - 1) * 3
 width = (cords * 128) / (depth / 12) / height
 width = round(width, 2)
 
-f"depth: {depth_w_gaps}\""
+f"depth: {round(depth_w_gaps/12, 2)}'"
 f"height: {height}'"
 f"width: {width}'"
